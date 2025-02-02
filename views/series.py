ba2f322ba53bf21bb5f9b7ff7f6f9c6a01e12196
Isleg <mikeismerio@gmail.com>
2025-02-01 19:15:27 -0600

    Update series.py

--- views/series.py
+++ views/series.py
@@ -1,10 +1,5 @@
-# views/series.py
 import streamlit as st
 
 def app():
     st.header("Página de Búsqueda de Series 📺")
-
     st.markdown("Aquí podrás buscar series más adelante.")
-    
-    if st.button("Volver al inicio"):
-        st.session_state.page = "home"
