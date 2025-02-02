02dce5749abb8f7ab2272d8f2c57f669fdc83fe7
Isleg <mikeismerio@gmail.com>
2025-02-01 18:46:46 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 def get_image_url(poster_path):
     """Devuelve la URL de la imagen o una imagen de marcador de posición."""
-    if pd.notna(poster_path):
+    if pd.notna(poster_path) and poster_path:
         return f"https://image.tmdb.org/t/p/w500{poster_path}"
     return "https://via.placeholder.com/200?text=No+Image"
