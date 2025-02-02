fabfca16236ce20ea5b5d022431ce75500c79c29
Isleg <mikeismerio@gmail.com>
2025-02-01 18:56:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import streamlit as st
 # Configurar la página
 st.set_page_config(page_title="Inicio", page_icon="🏠", layout="wide")
 
-# Inicializar el estado de la sesión para navegación y show seleccionado
+# Inicializar el estado de la sesión para navegación
 if "page" not in st.session_state:
     st.session_state.page = "home"
-if "selected_movie" not in st.session_state:
-    st.session_state.selected_movie = None
 
 # Importar las vistas
-from views import home, details
+from views import home, series, movies
 
 # Enrutamiento según el valor en st.session_state.page
 if st.session_state.page == "home":
     home.app()
-elif st.session_state.page == "details":
-    details.app()
+elif st.session_state.page == "series":
+    series.app()
+elif st.session_state.page == "movies":
+    movies.app()
